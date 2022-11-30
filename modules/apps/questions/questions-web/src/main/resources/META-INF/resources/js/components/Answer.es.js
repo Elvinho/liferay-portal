422dbf2d3f11fd60b159dd5c55aba35c4d38850e
Elvinho <elvison.victor@liferay.com>
2022-11-30 15:29:25 -0300

    LPS-169827 [FE] SF

--- modules/apps/questions/questions-web/src/main/resources/META-INF/resources/js/components/Answer.es.js
+++ modules/apps/questions/questions-web/src/main/resources/META-INF/resources/js/components/Answer.es.js
@@ -205,7 +205,8 @@ export default withRouter(
 																			);
 																		}
 																	);
-																} else {
+																}
+																else {
 																	deleteAnswer(
 																		answer
 																	);
